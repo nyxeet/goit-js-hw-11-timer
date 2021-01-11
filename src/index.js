655d199492dc64c51b6dd7c6d32649ae70d1824c
nyxeet <nyxeet@ukr.net>
2021-01-11 10:37:46 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,37 +1,38 @@
 import './styles.css';
 
-const daysRef = document.querySelector('[data-value="days"]')
-const hoursRef = document.querySelector('[data-value="hours"]')
-const minutesRef = document.querySelector('[data-value="mins"]')
-const secondsRef = document.querySelector('[data-value="secs"]')
-
 class CountdownTimer {
   constructor({ selector, targetDate }) {
     this.selector = selector;
     this.targetDate = targetDate;
+    this.refs = {
+      daysRef: document.querySelector(`${this.selector} [data-value="days"]`),
+      hoursRef: document.querySelector(`${this.selector} [data-value="hours"]`),
+      minutesRef: document.querySelector(`${this.selector} [data-value="mins"]`),
+      secondsRef: document.querySelector(`${this.selector} [data-value="secs"]`),
+    }
   }
   start() {
     setInterval(() => {
       const currentTime = Date.now()
       const time = this.targetDate - currentTime;
-      updateClock(time);
+      this.updateClock(time);
     }, 1000)
   }
+  updateClock(time) {
+    const { daysRef, hoursRef, minutesRef, secondsRef } = this.refs;
+    const days = padDays(Math.floor(time / (1000 * 60 * 60 * 24)));
+    const hours = pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+    const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
+
+    daysRef.textContent = days;
+    hoursRef.textContent = hours;
+    minutesRef.textContent = mins;
+    secondsRef.textContent = secs;
+  }
+    
 }
 
-
-
-function updateClock(time) {
-  const days = padDays(Math.floor(time / (1000 * 60 * 60 * 24)));
-  const hours = pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
-  const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
-  const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
-
-  daysRef.textContent = days;
-  hoursRef.textContent = hours;
-  minutesRef.textContent = mins;
-  secondsRef.textContent = secs;
-}
 function pad(value) {
   return String(value).padStart(2, '0');
 }
@@ -44,4 +45,5 @@ const initTimer = new CountdownTimer({
   selector: '#timer-1',
   targetDate: new Date('Jan 21, 2021'),
 });
+
 initTimer.start();
